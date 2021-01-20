d73476b63e3d56ca8e37313a8e1e8795120243e2
Kevin Mueller <kmueller1138@gmail.com>
2021-01-20 16:32:12 +0100

    Update maze.spec.ts

--- test/maze.spec.ts
+++ test/maze.spec.ts
@@ -1,3 +1,20 @@
-import {Maze, Cell, BoundingBox, CellType, CellDimensions, MazeExtents} from "../src/maze";
+import { Maze, Cell, BoundingBox, CellType, CellDimensions, MazeExtents } from "../src/maze";
 import { assert } from 'chai';
 import 'mocha';
+
+describe('maze', () => {
+    describe('getCellType', () => {
+        it('should get the type of the specified cell', () => {
+            const testCellType: CellType = {
+                type: 'path',
+                value: 1,
+                label: 'Path',
+                class: 'mazePath',
+                bgColor: 'white',
+                isPath: true
+            };
+
+
+        });
+    });
+});
