c3c5565bce1bcbc2606ba3ee68f78e23d01cbb43
Kevin Mueller <kmueller1138@gmail.com>
2021-01-22 18:00:05 +0100

    Added unit test for getCellType

--- test/maze.spec.ts
+++ test/maze.spec.ts
@@ -3,6 +3,13 @@ import { assert } from 'chai';
 import 'mocha';
 
 describe('maze', () => {
+    const testMazeObj = {
+        width: 5,
+        height: 5,
+        cellDimensions: { "width": 32, "height": 32 },
+        cells: ["start", "path", "path", "path", "path", "wall", "wall", "path", "path", "path", "wall", "wall", "path", "path", "path", "wall", "wall", "path", "path", "path", "wall", "wall", "wall", "wall", "end"]
+    };
+
     describe('getCellType', () => {
         it('should get the type of the specified cell', () => {
             const testCellType: CellType = {
@@ -14,7 +21,24 @@ describe('maze', () => {
                 isPath: true
             };
 
+            const testMaze = Maze.fromJSON(testMazeObj);
+            const cellType = testMaze.getCellType(1, 2);
 
+            assert.deepEqual(cellType, testCellType);
+        });
+
+        it('should return null if row is less than 0', () => {
+            const testMaze = Maze.fromJSON(testMazeObj);
+            const cellType = testMaze.getCellType(-1, 0);
+
+            assert.isNull(cellType);
+        });
+
+        it('should return null if row is more than the number of rows', () => {
+            const testMaze = Maze.fromJSON(testMazeObj);
+            const cellType = testMaze.getCellType(5, 2);
+
+            assert.isNull(cellType);
         });
     });
 });
